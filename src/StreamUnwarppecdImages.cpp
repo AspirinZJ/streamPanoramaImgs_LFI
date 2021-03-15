ca1b6f2a003812563832d8638e598942a8c40b14
AspirinZJ <zhangmengwei1996@outlook.com>
2021-03-15 18:14:56 -0400

    Init the repo

---
+++ src/StreamUnwarppecdImages.cpp
@@ -0,0 +1,147 @@
+#include <iostream>
+#include <jsoncpp/json/json.h>
+#include <opencv2/opencv.hpp>
+
+#include "ImageWrapper.h"
+
+int centerX = 0;
+int centerY = 0;
+int dis = 0;
+int scaleRatio = 0;
+double topCutoff = 0;
+double bottomCutoff = 0;
+int recordedWidth, recordedHeight;
+int calibratedWidth, calibratedHeight;
+double recordRatioX, recordRatioY;
+// parameters for choosing ROI
+//unsigned int topMargin = 200, bottomMargin = 200;
+//unsigned int leftMargin = 300, rightMargin = 300;
+
+/**
+ * @brief 显示unwrapped的图像并储存
+ * @details 按s储存图片，按q退出程序
+ * @param camNum 相机设备的id，从0开始
+ * @param imgSaveNum 起始的图片编号，默认为0
+ * @param scale 尺度因子，默认为1
+ */
+void streamImage(int camNum, unsigned int imgSaveNum = 0, unsigned int scale = 1);
+
+int main(int argc, char *argv[])
+{
+	if (2 != argc)
+	{
+		std::cerr << "Error: not enough arguments.\nUsage: [" << argv[0] << "] [initial image number to save]\n";
+		return 1;
+	}
+
+	Json::Value rootValue;
+	std::ifstream ifsRoot("../config/lawn_mover.json", std::ios::binary);
+	if (!ifsRoot.is_open())
+	{
+		std::cerr << "Error: cannot open root file!\n";
+		return -1;
+	}
+	ifsRoot >> rootValue;
+	ifsRoot.close();
+
+	std::string cameraConfig = rootValue["current_camera"].asString();
+	recordedWidth = rootValue["camera_record_res"][0].asInt();
+	recordedHeight = rootValue["camera_record_res"][1].asInt();
+
+	Json::Value cameraValue;
+	std::ifstream ifsCamera("../config/" + cameraConfig + ".json");
+	if (!ifsCamera.is_open())
+	{
+		std::cerr << "Error: cannot open camera file!\n";
+		return -1;
+	}
+	ifsCamera >> cameraValue;
+	ifsCamera.close();
+
+	scaleRatio = cameraValue[1]["display_ratio"].asInt();
+	calibratedWidth = cameraValue[1]["raw_resolution"][0].asInt();
+	calibratedHeight = cameraValue[1]["raw_resolution"][1].asInt();
+	recordRatioX = (double) calibratedWidth / recordedWidth;
+	recordRatioY = (double) calibratedHeight / recordedHeight;
+
+	if (recordRatioX != recordRatioY)
+	{
+		std::cerr << "Error: Recorde image aspect ratio different from calibrated file!" << std::endl;
+		return -1;
+	}
+
+	dis = (int) ((double) cameraValue[1]["distance"].asInt() * 2 / scaleRatio / recordRatioX);
+	centerX = (int) ((double) cameraValue[1]["center_x"].asInt() / scaleRatio / recordRatioX);
+	centerY = (int) ((double) cameraValue[1]["center_y"].asInt() / scaleRatio / recordRatioY);
+
+	topCutoff = cameraValue[1]["top_cutoff"].asDouble();
+	bottomCutoff = cameraValue[1]["bottom_cutoff"].asDouble();
+
+	unsigned int imgSaveNum = std::stoi(argv[1]);
+	streamImage(0, imgSaveNum);
+
+	return 0;
+}
+
+void streamImage(int camNum, unsigned int imgSaveNum, unsigned int scale)
+{
+	auto video = cv::VideoCapture(camNum);
+	if (!video.isOpened())
+	{
+		std::cerr << &"Error: cannot open camera: "[camNum] << std::endl;
+		return;
+	}
+
+	// set openCV capture resolution
+	video.set(CV_CAP_PROP_FRAME_HEIGHT, 1080);
+	video.set(CV_CAP_PROP_FRAME_WIDTH, 1920);
+
+	cv::Mat frame, resizedImage;
+
+	ImageWrapper wrapper;
+	bool res = wrapper.initImageWrapper(dis, dis);
+	if (!res)
+	{
+		std::cerr << "Error: Failed to init image wrapper!" << std::endl;
+		return;
+	}
+	std::cout << "Image wrapper init success!\n";
+
+	bool isNotEnd = true;
+
+	while (isNotEnd)
+	{
+		isNotEnd = video.read(frame);
+		if (frame.empty()) { continue; }
+		if (1 != scale) { cv::resize(frame, frame, cv::Size(), scale, scale, cv::INTER_LINEAR); }
+
+		cv::resize(frame, resizedImage, cv::Size((int) (frame.cols / scaleRatio), (int) (frame.rows / scaleRatio)));
+
+		cv::Rect myROI(centerX - dis / 2, centerY - dis / 2, dis, dis);
+		cv::Mat croppedImage = resizedImage(myROI);
+
+		cv::Mat image = wrapper.getImage(croppedImage);
+
+		cv::Rect myROI2(0, (int) (image.rows / topCutoff), image.cols, (int) (image.rows / bottomCutoff));
+		cv::Mat finalImage = image(myROI2);
+
+		cv::imshow("final", finalImage);
+
+		// cut margins out
+		// cv::Rect finalROI(leftMargin, topMargin, finalImage.rows - leftMargin- rightMargin, finalImage.cols-topMargin-bottomMargin);
+		// finalImage = finalImage(finalROI);
+
+		// save unwrapped images
+		if (cv::waitKey(50) == 's' || cv::waitKey(50) == 'S')
+		{
+			std::string imName = "../raw_images/" + std::to_string(++imgSaveNum) + ".png";
+			cv::imwrite(imName, finalImage);
+			std::cout << "Wrote to: " << imName << std::endl;
+		}
+		else if (cv::waitKey(50) == 'q' || cv::waitKey(50) == 'Q')
+		{
+			std::cout << "Quitting program." << std::endl;
+			return;
+		}
+	}
+}
---
+++ src/streamCameraVideo.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+#define DELAY 1
+#define VIDEO_WIDTH 3840
+#define VIDEO_HEIGHT 2160
+
+int main(int, char **)
+{
+	cv::Mat frame;
+	cv::VideoCapture videoCapture; // initialize video capture
+	const int deviceID = 0; // 0 = open default camera
+	const int apiID = cv::CAP_ANY;    // 0 = auto detect default API
+	int capNum = 0; // capture counting number
+
+	videoCapture.open(deviceID, apiID);    // open selected camera using selected API
+	if (!videoCapture.isOpened())
+	{
+		std::cerr << "Error: cannot open device!/n";
+		return -1;
+	}
+
+	// set the width and height of the frame, otherwise the default will be 640x480
+	videoCapture.set(CV_CAP_PROP_FRAME_WIDTH, VIDEO_WIDTH);
+	videoCapture.set(CV_CAP_PROP_FRAME_HEIGHT, VIDEO_HEIGHT);
+
+	// set framerate of the videoCapture
+	videoCapture.set(CV_CAP_PROP_FPS, 30);
+
+//	int fourcc = cv::VideoWriter::fourcc('M', 'J', 'P', 'G'); videoCapture.set(CV_CAP_PROP_FOURCC, fourcc);
+
+	std::cout << "Start grabbing: \n Press 's' to save image and press 'q' to terminate.\n";
+
+	cv::namedWindow("Live", cv::WINDOW_AUTOSIZE);
+	while (true)
+	{
+		// wait for a new frame from camera and store it into frame
+		videoCapture.read(frame); // or videoCapture >> frame;
+		if (frame.empty()) { continue; }
+
+		cv::imshow("Live", frame);
+		if (cv::waitKey(DELAY) == 's' || cv::waitKey(DELAY) == 'S')
+		{
+			std::string imName = "../raw_images/" + std::to_string(++capNum) + ".png";
+			cv::imwrite(imName, frame);
+			std::cout << "Wrote to : " << imName << std::endl;
+		}
+		if (cv::waitKey(DELAY) == 'q' || cv::waitKey(DELAY) == 'Q') { break; }
+	}
+	videoCapture.release();
+	// the camera will be destroyed automatically in VideoCapture destructor.
+	return 0;
+}
